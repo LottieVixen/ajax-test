e46b238e1d174087e1886f322e6384e4159aa080
Charlotte Fields <foxsan48@gmail.com>
2015-08-27 00:43:58 +1000

    added Async JS from 5th tut and commented

--- foodstore.js
+++ foodstore.js
@@ -1,9 +1,9 @@
 var xmlHttp = createXmlHttpRequestObjext();
-
+//setup http request
 function createXmlHttpRequestObjext() {
 	var xmlHttp;
 
-	if(window.ActiveXObject) {
+	if(window.ActiveXObject) {  /// if IE
 		try {
 			xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
 		}catch(e){
@@ -21,4 +21,15 @@ function createXmlHttpRequestObjext() {
 		alert("Can't create that object");
 	else
 		return xmlHttp;
+}
+
+function process(){
+	if(xmlHttp.readyState==4 || xmlHttp.readyState==0){ //if async ready
+		food = encodeURIComponent(document.getElementById('userInput').value); //get value
+		xmlHttp.open("GET", "foodstore.php?food="+food,true); //with get send food, async true
+		xml.onreadystatechange = handleServerResponse;
+		xmlHttp.send(null); //send get requ
+	} else {
+		setTimeout('process()',1000); //recall process each second
+	}
 }
