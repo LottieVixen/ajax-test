cda930a546e0bd45e6471d3dcfe7387b0f420756
Charlotte Fields <foxsan48@gmail.com>
2015-08-27 01:55:13 +1000

    re-added xmlHeaders

--- foodstore.php
+++ foodstore.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/xml');
-echo '<?xml version="1.0" encoding="UTF-8" standalone="YES" ?>';
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
 
 echo '<response>';
 	$food = $_GET['food'];
