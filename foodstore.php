c914cff14e65feef0a28d007ebca7e960538d1b1
Charlotte Fields <foxsan48@gmail.com>
2015-08-27 00:23:20 +1000

    added php code from 3rd tut

--- foodstore.php
+++ foodstore.php
@@ -3,6 +3,13 @@ header('Content-Type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8" standalone="YES" ?>';
 
 echo '<response>';
-
+	$food = $_GET['food'];
+	$foodArray = array('tuna','bacon','beef','loaf','ham');
+	if(in_array($food,$foodArray))
+		echo 'We do have '.$food.'!';
+	elseif($food=='')
+		echo 'Enter a food you silly bugger';
+	else
+		echo 'We don\'t have '.$food.', sorry mate.';
 echo '</response>';
 ?>
